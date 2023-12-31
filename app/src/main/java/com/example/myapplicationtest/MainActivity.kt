ec1e911ea9a1f1dd64937767401be275c6df550f
hanif415 <hanifamrullah42@gmail.com>
2023-12-31 15:51:37 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.kt
@@ -0,0 +1,178 @@
+package com.example.myapplicationtest
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.OutputFileOptions
+import androidx.camera.core.ImageCapture.OutputFileResults
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Executors
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var capture: ImageButton
+    lateinit var toggleFlash: ImageButton
+    lateinit var flipCamera: ImageButton
+    lateinit var previewView: PreviewView
+    private var cameraFacing = CameraSelector.LENS_FACING_BACK
+
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                startCamera(cameraFacing)
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        previewView = findViewById(R.id.cameraPreview);
+        capture = findViewById(R.id.capture);
+        toggleFlash = findViewById(R.id.toggleFlash);
+        flipCamera = findViewById(R.id.flipCamera);
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            activityResultLauncher.launch(Manifest.permission.CAMERA)
+        } else {
+            startCamera(cameraFacing)
+        }
+
+        flipCamera.setOnClickListener(View.OnClickListener {
+            cameraFacing = if (cameraFacing == CameraSelector.LENS_FACING_FRONT) {
+                CameraSelector.LENS_FACING_FRONT
+            } else {
+                CameraSelector.LENS_FACING_BACK
+            }
+            startCamera(cameraFacing)
+        })
+
+    }
+
+    private fun startCamera(cameraFacing: Int) {
+        val aspectRatio: Int = aspectRatio(previewView.width, previewView.height)
+        val listenableFuture: ListenableFuture<ProcessCameraProvider> =
+            ProcessCameraProvider.getInstance(this)
+
+        listenableFuture.addListener({
+            try {
+                val cameraProvider: ProcessCameraProvider =
+                    listenableFuture.get() as ProcessCameraProvider
+
+                val preview =
+                    Preview.Builder().setTargetAspectRatio(aspectRatio).build()
+
+                val imageCapture = ImageCapture.Builder()
+                    .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                    .setTargetRotation(windowManager.defaultDisplay.rotation).build()
+
+                val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(cameraFacing).build()
+                cameraProvider.unbindAll()
+
+                val camera: Camera =
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+
+                capture.setOnClickListener {
+                    if (ContextCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    }
+                    takePicture(imageCapture)
+                }
+                toggleFlash.setOnClickListener { setFlashIcon(camera) }
+
+                preview.setSurfaceProvider(previewView.surfaceProvider)
+
+            } catch (e: ExecutionException) {
+                e.printStackTrace()
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    fun takePicture(imageCapture: ImageCapture) {
+        val file = File(getExternalFilesDir(null), System.currentTimeMillis().toString() + ".jpg")
+        val outputFileOptions = OutputFileOptions.Builder(file).build()
+        imageCapture.takePicture(
+            outputFileOptions,
+            Executors.newCachedThreadPool(),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: OutputFileResults) {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Image saved at: " + file.path,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    startCamera(cameraFacing)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Failed to save: " + exception.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    startCamera(cameraFacing)
+                }
+            })
+    }
+
+    private fun setFlashIcon(camera: Camera) {
+        if (camera.cameraInfo.hasFlashUnit()) {
+            if (camera.cameraInfo.torchState.value == 0) {
+                camera.cameraControl.enableTorch(true)
+                toggleFlash.setImageResource(R.drawable.baseline_flash_off_24)
+            } else {
+                camera.cameraControl.enableTorch(false)
+                toggleFlash.setImageResource(R.drawable.baseline_flash_on_24)
+            }
+        } else {
+            runOnUiThread {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Flash is not available currently",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = Math.max(width, height).toDouble() / Math.min(width, height)
+        if (Math.abs(previewRatio - 4.0 / 3.0) <= Math.abs(previewRatio - 16.0 / 9.0)) {
+            return AspectRatio.RATIO_4_3;
+        }
+        return AspectRatio.RATIO_16_9;
+    }
+}
